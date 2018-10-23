adfbae207d107bb14326f443c6441bd7d0b21285
Daniel Mundim <dmundim.cr@gmail.com>
2018-10-23 12:06:12 -0300

    Update youtube-player.js

--- js/youtube-player.js
+++ js/youtube-player.js
@@ -1,14 +1,3 @@
-﻿// Gets all divs with "youtube" class and:
-// 1. Gets the corresponding thumbnail image through "embed" property, to show on the div without requesting the video itself to youtube servers.
-// 2. Assynchronously appends the image to the div.
-// 3. Adds click event to load the video iframe, making the request for full video only once user clicks.
-//
-// Div Properties:
-// - embed: youtube video URL 
-// - thumbnailquality: thumbnail quality (standard, medium, high, maximum)
-//
-// Atenttion for thumbnail-quality, be aware that not all youtube videos have thumbnails for all qualities available.
-
 document.addEventListener("DOMContentLoaded", function(event) { 
     var youtube = document.querySelectorAll(".youtube");
 
