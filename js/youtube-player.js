57698e5f5f42cba04d2a44dc663286936d94f92e
Daniel Mundim <dmundim.cr@gmail.com>
2018-11-27 12:03:17 -0200

    Update youtube-player.js
    fix bug that didn't get the right video id once user clicked

--- js/youtube-player.js
+++ js/youtube-player.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
             iframe.setAttribute("frameborder", "0");
             iframe.setAttribute("allowfullscreen", "");
-            iframe.setAttribute("src", "https://www.youtube.com/embed/" + vid + "?rel=0&autoplay=1");
+            iframe.setAttribute("src", "https://www.youtube.com/embed/" + getVideoId(youtube[i].dataset.embed) + "?rel=0&autoplay=1");
 
             this.innerHTML = "";
             this.appendChild(iframe);
